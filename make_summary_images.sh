ec9e221fbdfd2a48638e69640166aaefb20181b6
Mauricio J. Lozano <maurijlozano@gmail.com>
2019-10-30 10:34:51 -0300

    Add files via upload

--- make_summary_images.sh
+++ make_summary_images.sh
@@ -40,10 +40,10 @@ ls -d */ | while read F; do
     FS=$(echo "${FE}" | sed -e 's/\(^[A-Za-z0-9]*\)[_ ]\(.\)[A-Za-z0-9]*[_ ]\([A-Za-z0-9]*\).*/\1_\2.\3/')
     FN=$(echo "${FS}" | sed -e 's/\///')
 
-    if [[ ! -f "./${F}/"CA_genes_ws.svg || ! -f "./${F}/"CA_genes2_ws.svg || ! -f "./${F}/"CA_codons_ws.svg ]] ; then
+    if [[ ! -f "./${F}/"CA_genes_GNM.svg || ! -f "./${F}/"CA_codons_GNM.svg ]] ; then
         echo 'You need to run coa.sh for CA analysis first.'
         if [[ ! -f "./${F}/"IvsD_modal.svg || ! -f "./${F}/"GC3vsD_modal.svg ]] ; then
-               echo 'You need to run Index_modal.sh | GC3_modal.sh first.'
+               echo 'You need to run Index_Modal.sh | GC3_modal.sh first.'
                continue
         fi
         svg_stack.py --direction=h --margin=30 "./${F}/"IvsD_modal.svg "./${F}/"GC3vsD_modal.svg  > "${FN}".svg
@@ -51,17 +51,17 @@ ls -d */ | while read F; do
     else
         if [[ ! -f "./${F}/"IvsD_modal.svg || ! -f "./${F}/"GC3vsD_modal.svg ]] ; then
                echo 'You need to run Index_modal.sh | GC3_modal.sh first.'
-        svg_stack.py --direction=h --margin=30 "./${F}/"CA_genes_ws.svg "./${F}/"CA_genes2_ws.svg "./${F}/"CA_codons_ws.svg  > "${FN}".svg
+        svg_stack.py --direction=h --margin=30 "./${F}/"CA_genes_GNM.svg "./${F}/"CA_codons_GNM.svg  > "${FN}".svg
                continue
         fi
     fi
 
-    svg_stack.py --direction=h --margin=30 "./${F}/"CA_genes_ws.svg "./${F}/"CA_genes2_ws.svg "./${F}/"CA_codons_ws.svg  > 1.svg
+    svg_stack.py --direction=h --margin=30 "./${F}/"CA_genes_GNM.svg "./${F}/"CA_codons_GNM.svg  > 1.svg
     svg_stack.py --direction=h --margin=30 "./${F}/"IvsD_modal.svg "./${F}/"GC3vsD_modal.svg  > 2.svg
     svg_stack.py --direction=v --margin=50 1.svg 2.svg  > "${FN}".svg
     
-    if [[ -f "./${F}/"CA_genes2_ws_woPHE.svg && -f "./${F}/"IvsD_modal_woPHE.svg ]] ; then
-    svg_stack.py --direction=h --margin=30 "./${F}/"CA_genes_ws.svg "./${F}/"CA_genes2_ws.svg "./${F}/"CA_genes2_ws_woPHE.svg "./${F}/"CA_codons_ws.svg  > 3.svg
+    if [[ -f "./${F}/"CA_genes2_GNM_woPHE.svg && -f "./${F}/"IvsD_modal_woPHE.svg ]] ; then
+    svg_stack.py --direction=h --margin=30 "./${F}/"CA_genes_GNM.svg "./${F}/"CA_genes2_GNM_woPHE.svg "./${F}/"CA_codons_GNM.svg  > 3.svg
     svg_stack.py --direction=h --margin=30 "./${F}/"IvsD_modal.svg "./${F}/"IvsD_modal_woPHE.svg  > 4.svg
     svg_stack.py --direction=h --margin=30 "./${F}/"GC3vsD_modal.svg "./${F}/"GC3vsD_modal_woPHE.svg > 5.svg
     svg_stack.py --direction=v --margin=50 3.svg 4.svg 5.svg > "${FN}"2.svg
